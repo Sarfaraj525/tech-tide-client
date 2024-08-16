8e1e6c6a7c30cddde58732c12213b4c36dc2320f
Sarfaraj525 <sarfaraj525@gmail.com>
2024-08-16 10:02:32 +0600

    products show

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,24 @@
+import { useLoaderData } from "react-router-dom";
+import ProductCard from "../../ProductCard";
 
 
 const Home = () => {
+
+    const products = useLoaderData();
+
     return (
-        <div>
-            <h2>this is home</h2>
+        <div className="min-h-lvh">
+            <div className="m-20">
+            <h1 className="text-6xl text-orange-600 text-center">Tech Products: {products.length}</h1>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 px-4">
+            {
+                products.map(products => <ProductCard
+                key={products._id}
+                products={products}
+                ></ProductCard>)
+            }
+            </div>
+            </div>
         </div>
     );
 };
---
+++ src/pages/ProductCard.jsx
@@ -0,0 +1,34 @@
+
+
+const ProductCard = ({products}) => {
+
+    const {
+        ProductName, ProductImage, Description, PriceInTaka, Category, Ratings, ProductCreationDate, ProductCreationTime} = products;
+
+    return (
+        <div className="card bg-base-100 w-96 shadow-xl">
+  <figure>
+    <img
+      src={ProductImage}
+      alt="Mobile" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">
+      Name: {ProductName}
+      <div className="badge badge-secondary bg-orange-600">{Ratings}</div>
+    </h2>
+    <p>{Description}</p>
+    <div className="card-actions justify-end">
+      <div className="badge badge-outline">{Category}</div>
+      <div className="badge badge-outline">{PriceInTaka}/=</div>
+    </div>
+    <div className="card-actions justify-end">
+      <div className="badge badge-outline">Creation Date: {ProductCreationDate}</div>
+      <div className="badge badge-outline">{ProductCreationTime}</div>
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default ProductCard;
