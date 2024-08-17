46a2ce64647310acf88d39a1c3cbd2981b5ccf88
Sarfaraj525 <sarfaraj525@gmail.com>
2024-08-17 12:40:08 +0600

    sorting fix

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -7,47 +7,79 @@ const Home = () => {
   const [products, setProducts] = useState([]);
   const [sort, setSort] = useState("priceLowHigh");
   const [show, setShow] = useState(false);
-  const [brand, setBrand]= useState("");
-  const [cat, setCat]= useState("");
-  const [minPrice, setMinPrice]= useState("");
-  const [maxPrice, setMaxPrice]= useState("");
+  const [brand, setBrand] = useState("");
+  const [cat, setCat] = useState("");
+  const [minPrice, setMinPrice] = useState("");
+  const [maxPrice, setMaxPrice] = useState("");
+  const [totalPage, setTotalPage] = useState("");
+  const [currentPage, setCurrentPage] = useState(0);
+
   useEffect(() => {
-    fetch("http://localhost:5000/products")
+    fetch(
+      `https://tech-tide-server.vercel.app/products/?currentPage=${currentPage}`
+    )
       .then((data) => data.json())
       .then((res) => setProducts(res));
-  }, []);
+  }, [currentPage]);
 
   const handleSearch = (e) => {
-    fetch(`http://localhost:5000/products/search/${e.target.value}`)
+    fetch(
+      `https://tech-tide-server.vercel.app/products/search/${e.target.value}`
+    )
       .then((data) => data.json())
       .then((res) => setProducts(res));
   };
 
   useEffect(() => {
-    fetch(`http://localhost:5000/products/${sort}`)
-      .then((data) => data.json())
-      .then((res) => setProducts(res));
-  }, [sort]);
-
-  useEffect(() => {
-    fetch(`http://localhost:5000/product/Categorization/?brand=${brand}&category=${cat}&maxPrice=${maxPrice}&minPrice=${minPrice}`)
+    fetch(
+      `https://tech-tide-server.vercel.app/product/Categorization/?brand=${brand}&category=${cat}&maxPrice=${maxPrice}&minPrice=${minPrice}`
+    )
       .then((data) => data.json())
       .then((res) => setProducts(res));
   }, [brand, cat, minPrice, maxPrice]);
-  
-  
-// console.log(maxPrice, minPrice );
 
+  useEffect(() => {
+    fetch("https://tech-tide-server.vercel.app/totalProducts")
+      .then((data) => data.json())
+      .then((res) => setTotalPage(res.count));
 
+    //   console.log(totalPage);
+  }, []);
+
+  const pages = Math.ceil(totalPage / 9);
+  let makePage = [];
+  for (let index = 0; index < pages; index++) {
+    makePage.push(index);
+  }
+
+  // console.log(totalPage, makePage);
+  // console.log(maxPrice, minPrice );
+  const handlePrevious = () => {
+    if (0 < currentPage) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+  const handleNext = () => {
+    if (currentPage < makePage.length - 1) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+  // console.log(currentPage);
+
+  const handleSort = (opt) => {
+    fetch(`http://localhost:5000/products/${opt}`)
+      .then((data) => data.json())
+      .then((res) => setProducts(res));
+  };
 
   return (
     <div className="min-h-lvh">
       <div className="m-20">
-        <h1 className="text-6xl text-orange-600 text-center mb-8">
+        {/* <h1 className="text-6xl text-orange-600 text-center mb-8">
           Tech Products: {products.length}
-        </h1>
+        </h1> */}
 
-        <div className="flex justify-between ">
+        <div className="md:flex md:justify-between ">
           <input
             onChange={handleSearch}
             className="px-2 py-1 border-4 h-10 w-30"
@@ -64,27 +96,24 @@ const Home = () => {
               tabIndex={0}
               className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
             >
-              <li onClick={() => setSort("priceLowHigh")}>
+              <li onClick={() => handleSort("PriceInTaka,1")}>
                 <a>Low to High</a>
               </li>
-              <li onClick={() => setSort("priceHighLow")}>
+              <li onClick={() => handleSort("PriceInTaka,-1")}>
                 <a>High to Low</a>
               </li>
-              <li onClick={() => setSort("dateNewest")}>
+              <li onClick={() => handleSort("ProductCreationDate,-1")}>
                 <a>Newest first</a>
               </li>
             </ul>
           </div>
-
-
-
 
           {/* Categorization */}
           <div className="pr-28">
             <div
               onClick={() => setShow(!show)}
               role="button"
-              className="btn m-1 relative left-12 pr-20"
+              className="btn m-1 relative  pr-20"
             >
               Categorization
             </div>
@@ -95,21 +124,18 @@ const Home = () => {
             >
               <li>
                 <div className="dropdown dropdown-right">
-
                   <div>
                     <label
                       htmlFor="HeadlineAct"
                       className="block text-sm font-medium text-gray-900"
                     >
-                      
                       Brand Name
                     </label>
 
                     <select
                       name="HeadlineAct"
                       id="HeadlineAct"
-                      onChange={(e)=>setBrand(e.target.value)
-                      }
+                      onChange={(e) => setBrand(e.target.value)}
                       className="mt-1.5 w-full rounded-lg border-gray-300 text-gray-700 sm:text-sm"
                     >
                       <option value="">Please select</option>
@@ -146,8 +172,6 @@ const Home = () => {
                 </div>
               </li>
               <li>
-
-
                 <div className="dropdown dropdown-right">
                   <div tabIndex={0} role="button" className="btn m-1">
                     Category Name
@@ -156,16 +180,16 @@ const Home = () => {
                     tabIndex={0}
                     className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
                   >
-                    <li onClick={()=>setCat("Smartphone")}>
+                    <li onClick={() => setCat("Smartphone")}>
                       <a>Smartphone</a>
+                    </li>
+                    <li onClick={() => setCat("Gaming Smartphone")}>
                       <a>Gaming Smartphone</a>
                     </li>
                   </ul>
                 </div>
               </li>
               <li>
-
-
                 <div className="dropdown dropdown-right">
                   <div tabIndex={0} role="button" className="btn m-1">
                     Price Range
@@ -174,22 +198,36 @@ const Home = () => {
                     tabIndex={0}
                     className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
                   >
-                    <li onClick={()=>{setMinPrice(1), setMaxPrice(50000)}}>
+                    <li
+                      onClick={() => {
+                        setMinPrice(1), setMaxPrice(50000);
+                      }}
+                    >
                       <a>1-50000</a>
                     </li>
-                    <li onClick={()=>{setMinPrice(50000), setMaxPrice(100000)}}>
+                    <li
+                      onClick={() => {
+                        setMinPrice(50000), setMaxPrice(100000);
+                      }}
+                    >
                       <a>50000-100000</a>
                     </li>
-                    <li onClick={()=>{setMinPrice(100000), setMaxPrice(150000)}}>
+                    <li
+                      onClick={() => {
+                        setMinPrice(100000), setMaxPrice(150000);
+                      }}
+                    >
                       <a>100000-150000</a>
                     </li>
-                    <li onClick={()=>{setMinPrice(150000), setMaxPrice(200000)}}>
+                    <li
+                      onClick={() => {
+                        setMinPrice(150000), setMaxPrice(200000);
+                      }}
+                    >
                       <a>150000-200000</a>
                     </li>
                   </ul>
                 </div>
-
-
               </li>
             </ul>
           </div>
@@ -200,6 +238,36 @@ const Home = () => {
             <ProductCard key={products._id} products={products}></ProductCard>
           ))}
         </div>
+      </div>
+
+      {/* Pagenation */}
+      <div className="join grid-cols-2 mx-auto mt-16 flex justify-center mb-16">
+        <button
+          onClick={handlePrevious}
+          className="join-item btn btn-outline mr-1"
+        >
+          Previous page
+        </button>
+        <div className="join">
+          {makePage.map((page, i) => (
+            <button
+              type="button"
+              key={page}
+              onClick={() => setCurrentPage(page)}
+              className={`
+                                    btn btn-square ${
+                                      currentPage != i
+                                        ? "bg-orange-600"
+                                        : " bg-white "
+                                    } `}
+            >
+              {page + 1}
+            </button>
+          ))}
+        </div>
+        <button onClick={handleNext} className="join-item btn btn-outline">
+          Next
+        </button>
       </div>
     </div>
   );
